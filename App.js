4e5fd3d387f717b88026d84f2170095ee2f09d6b
Ebiperre <iyorop@yahoo.com>
2022-01-24 15:16:51 +0100

    updated

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Keyboard, StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+
   return (
     <View style={styles.container}>
       
@@ -13,12 +29,32 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          <Task text= {'Task 1'}/>
-          <Task text = {'Task 2'}/>
-          <Task/>
-          <Task/>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task  text={item}/>
+                </TouchableOpacity>
+              ) 
+            })
+          }
+          {/* <Task text= {'Task 1'}/>
+          <Task text = {'Task 2'}/> */}
         </View>
       </View>
+
+      {/* Write a task section */}
+      <KeyboardAvoidingView 
+      behavior = {Platform.OS === "ios"?"padding":"height"}
+      style ={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()} >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+        </KeyboardAvoidingView>
     </View>
   );
 }
@@ -40,4 +76,33 @@ sectionTitle: {
 items: {
   marginTop: 20,
 },
+writeTaskWrapper: {
+  position: 'absolute',
+  bottom: 60,
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+},
+input: {
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  backgroundColor: '#fff',
+  borderRadius: 60,
+  borderColor: '#c0c0c0',
+  borderWidth: 1,
+  width: 250,
+  
+},
+addWrapper: {
+  width: 60,
+  height: 60,
+  backgroundColor: '#fff',
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: '#c0c0c0',
+  borderWidth: 1,
+},
+addText: {},
 });
